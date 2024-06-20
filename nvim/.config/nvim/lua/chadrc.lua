210162a1083d9ca8f886de4567d37d6d465f0aaa
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-06-20 23:11:44 +0300

    Fixed custom colors

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -1,39 +1,32 @@
--- This file  needs to have same structure as nvconfig.lua
--- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
-
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-    theme = "material-darker",
+  theme = "material-darker",
 
-    hl_override = {
-        Comment = { italic = true },
-        ["@comment"] = { italic = true },
-    },
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
 
-    changed_themes = {
-        ["material-darker"] = {
-            base_30 = {
-                darker_black = "#000000",
-                black = "#000000",
-                black2 = "#000000",
-                one_bg = "#000000",
-                one_bg2 = "#000000",
-                one_bg3 = "#000000",
-                statusline_bg = "#000000",
-                lightbg = "#000000",
-                pmenu_bg = "#000000",
-            },
-            base_16 = {
-                base00 = "#000000",
-                base01 = "#000000",
-                base02 = "#000000",
-                base03 = "#000000",
-                base04 = "#000000",
-            },
-        },
+  changed_themes = {
+    ["material-darker"] = {
+      base_30 = {
+        darker_black = "#000000",
+        black2 = "#000000",
+        one_bg = "#000000",
+        one_bg2 = "#000000",
+        one_bg3 = "#000000",
+        statusline_bg = "#000000",
+        lightbg = "#000000",
+        pmenu_bg = "#ff6961",
+      },
+      base_16 = {
+        base00 = "#000000",
+        base01 = "#000000",
+      },
     },
+  },
 }
 
 return M
