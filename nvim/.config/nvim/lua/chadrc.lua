d020367b5c84b4b5443b423c4ad5369b82b052c5
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-07-01 16:06:53 +0300

    Cleaned up config

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -2,31 +2,26 @@
 local M = {}
 
 M.ui = {
-  theme = "material-darker",
+    theme = "material-darker",
 
-  hl_override = {
-    Comment = { italic = true },
-    ["@comment"] = { italic = true },
-  },
-
-  changed_themes = {
-    ["material-darker"] = {
-      base_30 = {
-        darker_black = "#000000",
-        black2 = "#000000",
-        one_bg = "#000000",
-        one_bg2 = "#000000",
-        one_bg3 = "#000000",
-        statusline_bg = "#000000",
-        lightbg = "#000000",
-        pmenu_bg = "#ff6961",
-      },
-      base_16 = {
-        base00 = "#000000",
-        base01 = "#000000",
-      },
+    hl_override = {
+        Comment = { italic = true },
+        ["@comment"] = { italic = true },
     },
-  },
+
+    changed_themes = {
+        ["material-darker"] = {
+            base_30 = {
+                darker_black = "#000000", -- e.g. nvim-tree and telescope bg
+                black2 = "#000000", -- Current line highlight
+                statusline_bg = "#000000",
+                pmenu_bg = "#ff6961",
+            },
+            base_16 = {
+                base00 = "#000000",
+            },
+        },
+    },
 }
 
 return M
