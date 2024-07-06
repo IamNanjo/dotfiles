a00bc503d261a0b9a7b3e5cfa8f1a9d02f0ecb59
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-07-06 19:30:40 +0300

    Added autotag, tabout and disabled mouse

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -54,4 +54,54 @@ return {
       }
     end,
   },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup {
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = true,
+        },
+      }
+    end,
+  },
+  {
+    "abecodes/tabout.nvim",
+    config = function()
+      require("tabout").setup {
+        tabkey = "<Tab>",
+        backwards_tabkey = "",
+        act_as_tab = true,
+        act_as_shift_tab = true,
+        default_tab = "<C-t>",
+        default_shift_tab = "<C-d>",
+        enable_backwards = false,
+        completion = false,
+        tabouts = {
+          { open = "'", close = "'" },
+          { open = '"', close = '"' },
+          { open = "`", close = "`" },
+          { open = "(", close = ")" },
+          { open = "[", close = "]" },
+          { open = "{", close = "}" },
+          { open = "<", close = ">" },
+        },
+      }
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/nvim-cmp",
+    },
+    event = "InsertCharPre",
+    priority = 1000,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      -- Disable default tab keybinding in LuaSnip
+      return {}
+    end,
+  },
 }
