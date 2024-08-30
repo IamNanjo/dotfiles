39f5517c25c52f5ddeaed130874d54aced994a02
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-08-30 21:53:16 +0300

    Disabled ensure_installed

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,13 +3,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-      ensure_installed = { "bash", "diff", "html", "css", "javascript", "typescript", "lua", "luadoc", "markdown" },
       auto_install = true,
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = { "ruby" },
       },
-      indent = { enable = true, disable = { "ruby" } },
+      indent = { enable = true },
     },
     config = function(_, opts)
       require("nvim-treesitter.install").prefer_git = true
