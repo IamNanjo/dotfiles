20738d72fc0ab25512a113d452b755cb28adb969
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-10-18 11:22:31 +0300

    Added neogit plugin

--- nvim/.config/nvim/lua/plugins/basic.lua
+++ nvim/.config/nvim/lua/plugins/basic.lua
@@ -71,4 +71,14 @@ return {
       },
     },
   },
+
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = true,
+  },
 }
