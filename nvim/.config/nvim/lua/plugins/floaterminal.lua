4ec30c2fc1ca182dd35199786529ce0c35adc889
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2024-12-29 16:09:28 +0200

    Fixed dev plugin directory name

--- nvim/.config/nvim/lua/plugins/floaterminal.lua
+++ nvim/.config/nvim/lua/plugins/floaterminal.lua
@@ -1,14 +1,13 @@
 return {
     {
         url = "https://github.com/IamNanjo/floaterm.nvim",
-        name = "floaterminal",
+        name = "floaterm",
         dev = true,
         event = "VeryLazy",
         keys = {
             {
                 "<space>tt",
                 ":Floaterminal<CR>",
-                { "n", "t" },
                 desc = "Toggle floating terminal",
             },
         },
