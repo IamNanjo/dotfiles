4c79b9bd4d012d20bf5c8a4db36874d1cdc5f6bd
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-01-09 17:06:41 +0200

    Use lazy syntax for key bindings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,21 +1,35 @@
 return {
     { -- Fuzzy Finder (files, lsp, etc)
         "nvim-telescope/telescope.nvim",
-        event = "VimEnter",
         branch = "0.1.x",
+        keys = {
+            { "<leader>sf", require("telescope.builtin").find_files, desc = "[S]earch [F]iles" },
+            { "<leader>ss", require("telescope.builtin").builtin, desc = "[S]earch [S]elect Telescope" },
+            { "<leader>sw", require("telescope.builtin").grep_string, desc = "[S]earch current [W]ord" },
+            { "<leader>sg", require("telescope.builtin").git_files, desc = "[S]earch [G]it" },
+            { "<leader>sG", require("telescope.builtin").live_grep, desc = "[S]earch [G]rep" },
+            { "<leader>sd", require("telescope.builtin").diagnostics, desc = "[S]earch [D]iagnostics" },
+            {
+                "<leader>s/",
+                function()
+                    require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({}))
+                end,
+                desc = "[/] Fuzzily search in current buffer",
+            },
+        },
         dependencies = {
             "nvim-lua/plenary.nvim",
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-
-                build = "make",
-
-                cond = function()
-                    return vim.fn.executable("make") == 1
-                end,
-            },
-            { "nvim-telescope/telescope-ui-select.nvim" },
             { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+            -- {
+            --     "nvim-telescope/telescope-fzf-native.nvim",
+            --
+            --     build = "make",
+            --
+            --     cond = function()
+            --         return vim.fn.executable("make") == 1
+            --     end,
+            -- },
+            -- { "nvim-telescope/telescope-ui-select.nvim" },
         },
         config = function()
             require("telescope").setup({
@@ -29,21 +43,6 @@ return {
             -- Enable Telescope extensions if they are installed
             pcall(require("telescope").load_extension, "fzf")
             pcall(require("telescope").load_extension, "ui-select")
-
-            -- See `:help telescope.builtin`
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-            vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-            vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-            vim.keymap.set("n", "<leader>sg", builtin.git_files, { desc = "[S]earch [G]it" })
-            vim.keymap.set("n", "<leader>sG", builtin.live_grep, { desc = "[S]earch [G]rep" })
-            vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-
-            -- Slightly advanced example of overriding default behavior and theme
-            vim.keymap.set("n", "<leader>s/", function()
-                -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({}))
-            end, { desc = "[/] Fuzzily search in current buffer" })
         end,
     },
 }
