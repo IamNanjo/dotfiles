977f1f823f143788044c57259a98bc0330227ddc
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-02-22 23:47:59 +0200

    Added type for plugin config

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -3,6 +3,8 @@
 return {
     {
         "folke/trouble.nvim",
+        ---@module "trouble"
+        ---@type trouble.Config
         opts = {
             auto_close = true,
             auto_jump = true,
@@ -24,31 +26,6 @@ return {
                 "<leader>cx",
                 "<cmd>Trouble diagnostics toggle<cr>",
                 desc = "Diagnostics (Trouble)",
-            },
-            {
-                "<leader>cX",
-                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-                desc = "Buffer Diagnostics (Trouble)",
-            },
-            {
-                "<leader>cs",
-                "<cmd>Trouble symbols toggle focus=false<cr>",
-                desc = "Symbols (Trouble)",
-            },
-            {
-                "<leader>cl",
-                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-                desc = "LSP Definitions / references / ... (Trouble)",
-            },
-            {
-                "<leader>cL",
-                "<cmd>Trouble loclist toggle<cr>",
-                desc = "Location List (Trouble)",
-            },
-            {
-                "<leader>cQ",
-                "<cmd>Trouble qflist toggle<cr>",
-                desc = "Quickfix List (Trouble)",
             },
         },
     },
