f334d8fe53ffbf2d39c10fd642978329627d8a8f
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-05-03 17:15:45 +0300

    feat: Added script for setting up home dirs

---
+++ homedir_setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+create_dirs() {
+	[ -z "$1" ] && exit 1
+
+	mkdir -pv \
+		"$1"/{.cache,.config,.local} \
+		"$1"/.config/{bash,bat,copyq,fastfetch,flameshot,fsh,ghostty,neomutt,zed,zsh} \
+		"$1"/.local/{bin,share,state} \
+		"$1"/.local/share/{oh-my-zsh,SpeedCrunch,zsh} \
+		"$1"/.local/share/SpeedCrunch/color-schemes
+}
+
+HOME_DIR="$HOME"
+SKEL_DIR="/etc/skel"
+
+create_dirs "$HOME_DIR"
+
+read -p "Do you also want to edit /etc/skel directory? (y/N) " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+	sudo bash -c "$(declare -f create_dirs); create_dirs \"$SKEL_DIR\""
+fi
+
+unset HOME_DIR SKEL_DIR
