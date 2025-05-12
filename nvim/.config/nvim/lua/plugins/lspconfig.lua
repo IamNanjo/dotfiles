a198b42b075477d0e9e170a368dffc33a9ab7866
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-05-12 18:36:49 +0300

    chore: Removed unused keymaps

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -31,24 +31,10 @@ return {
                     map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
                     map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
                     map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-                    map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-                    map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-                    map(
-                        "<leader>ws",
-                        require("telescope.builtin").lsp_dynamic_workspace_symbols,
-                        "[W]orkspace [S]ymbols"
-                    )
                     map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
                     map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
                     map("<C-S-Space>", vim.lsp.buf.hover, "Hover Documentation")
                     map("K", vim.lsp.buf.hover, "Hover Documentation")
-                    vim.keymap.set(
-                        "i",
-                        "<C-h>",
-                        vim.lsp.buf.signature_help,
-                        { buffer = event.buf, desc = "LSP: Signature help" }
-                    )
-                    map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
                     -- The following two autocommands are used to highlight references of the
                     -- word under your cursor when your cursor rests there for a little while.
@@ -79,15 +65,13 @@ return {
                 end,
             })
 
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+            ---@class LspServerConfig
+            ---@property cmd table? Override the default command used to start the server
+            ---@property filetypes table? Override the default list of associated filetypes for the server
+            ---@property capabilities table? Override fields in capabilities. Can be used to disable certain LSP features.
+            ---@property settings table? Override the default settings passed when initializing the server.
 
-            --  Add any additional override configuration in the following tables. Available keys are:
-            --  - cmd (table): Override the default command used to start the server
-            --  - filetypes (table): Override the default list of associated filetypes for the server
-            --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-            --  - settings (table): Override the default settings passed when initializing the server.
-            local lspconfig = require("lspconfig")
+            ---@type table<string, LspServerConfig>
             local servers = {
                 html = {},
                 emmet_language_server = {
@@ -128,20 +112,16 @@ return {
                     },
                     filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
                 },
-                volar = {},
             }
 
-            local ensure_installed = vim.tbl_keys(servers or {})
-            vim.list_extend(ensure_installed, {
-                "stylua",
-                "prettier",
-            })
-
-            require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
+            local ensure_installed = vim.tbl_keys(servers)
+            vim.list_extend(ensure_installed, { "prettier", "stylua", "volar" })
 
             for k, v in pairs(servers) do
                 vim.lsp.config(k, v)
             end
+
+            require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
         end,
     },
 }
