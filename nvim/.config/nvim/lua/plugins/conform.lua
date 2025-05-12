fe55e91a56fcecd99f2d5193b73a3402223b6780
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-05-12 18:36:34 +0300

    feat: No autoformat on save

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -22,7 +22,7 @@ return {
 
             local opts = {
                 notify_on_error = false,
-                format_on_save = true,
+                format_on_save = false,
                 formatters_by_ft = { lua = { "stylua" } },
                 formatters = {},
             }
