1c9ad08d11b4194f02a551efedf39633e3cf296a
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-05-13 23:48:16 +0300

    feat: Disable highlighting of words under cursor

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -35,33 +35,6 @@ return {
                     map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
                     map("<C-S-Space>", vim.lsp.buf.hover, "Hover Documentation")
                     map("K", vim.lsp.buf.hover, "Hover Documentation")
-
-                    -- The following two autocommands are used to highlight references of the
-                    -- word under your cursor when your cursor rests there for a little while.
-                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client.server_capabilities.documentHighlightProvider then
-                        local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-                        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                            buffer = event.buf,
-                            group = highlight_augroup,
-                            callback = vim.lsp.buf.document_highlight,
-                        })
-
-                        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                            buffer = event.buf,
-                            group = highlight_augroup,
-                            callback = vim.lsp.buf.clear_references,
-                        })
-
-                        vim.api.nvim_create_autocmd("LspDetach", {
-                            group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-                            callback = function(event2)
-                                vim.lsp.buf.clear_references()
-                                vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
-                            end,
-                        })
-                    end
                 end,
             })
 
