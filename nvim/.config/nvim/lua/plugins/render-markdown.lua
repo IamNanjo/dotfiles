4427a060063558cdee9212925b55a08af08824b1
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-06-08 19:39:50 +0300

    feat: Render markdown

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,15 @@
+if vim.g.vscode then
+    return {}
+end
+
+---@module "lazy"
+---@type LazyPluginSpec[]
+return {
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
+}
