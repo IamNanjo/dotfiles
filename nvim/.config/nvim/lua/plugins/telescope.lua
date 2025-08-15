325a9dc2279f77f5118b7f6ea55860624eb1d484
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-08-15 10:00:06 +0300

    chore: Cleaned up unused code

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,13 +7,13 @@ end
 return {
     {
         "nvim-telescope/telescope.nvim",
-        branch = "master",
+        branch = "0.1.x",
         keys = {
-            { "<leader>sf", require("telescope.builtin").find_files,  desc = "[S]earch [F]iles" },
-            { "<leader>ss", require("telescope.builtin").builtin,     desc = "[S]earch [S]elect Telescope" },
+            { "<leader>sf", require("telescope.builtin").find_files, desc = "[S]earch [F]iles" },
+            { "<leader>ss", require("telescope.builtin").builtin, desc = "[S]earch [S]elect Telescope" },
             { "<leader>sw", require("telescope.builtin").grep_string, desc = "[S]earch current [W]ord" },
-            { "<leader>sg", require("telescope.builtin").git_files,   desc = "[S]earch [G]it" },
-            { "<leader>sG", require("telescope.builtin").live_grep,   desc = "[S]earch [G]rep" },
+            { "<leader>sg", require("telescope.builtin").git_files, desc = "[S]earch [G]it" },
+            { "<leader>sG", require("telescope.builtin").live_grep, desc = "[S]earch [G]rep" },
             { "<leader>sd", require("telescope.builtin").diagnostics, desc = "[S]earch [D]iagnostics" },
             {
                 "<leader>s/",
@@ -29,8 +29,6 @@ return {
         },
         config = function()
             require("telescope").setup({})
-
-            pcall(require("telescope").load_extension, "fzf")
         end,
     },
 }
