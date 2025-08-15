e69bbb2d7e43a92fbe99d84a5c50ae70c56fc617
IamNanjo <48948888+IamNanjo@users.noreply.github.com>
2025-08-15 09:59:19 +0300

    feat: Vim specific Lua LSP completions

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -52,7 +52,16 @@ return {
                 },
             },
 
-            sources = { default = { "lsp", "path", "snippets", "buffer" } },
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+                providers = {
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        score_offset = 100,
+                    },
+                },
+            },
 
             fuzzy = {
                 implementation = "prefer_rust",
